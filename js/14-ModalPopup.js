c7c5559a587e10187df74298328b28c3446f4127
Brian Bycynski <high5bri@hotmail.com>
2019-06-07 04:43:28 -0400

    finished section 32

---
+++ js/14-ModalPopup.js
@@ -0,0 +1,20 @@
+const btn = document.querySelectorAll('.modal');
+const modalWrapper = document.querySelector('.modalWrapper');
+const body = document.querySelector('body');
+btn.forEach(function(button) {
+  makeClick(button);
+});
+
+function makeClick(el) {
+  el.addEventListener('click', function() {
+    modalWrapper.classList.add('showModal');
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', function() {
+      return modalWrapper.classList.remove('showModal');
+    });
+    modalWrapper.addEventListener('click', function() {
+      return modalWrapper.classList.remove('showModal');
+    });
+    body.addEventListener('keydown', function(e) {});
+  });
+}
