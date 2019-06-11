1e63a64184879817f9394dbbcc9fd729aa641cf8
Brian Bycynski <high5bri@hotmail.com>
2019-06-11 05:04:00 -0400

    Finished section 42

---
+++ js/26-PlaceholderGenerator.js
@@ -0,0 +1,34 @@
+const sel = document.querySelector('select');
+const inpAll = document.querySelectorAll('input');
+const myImg = document.querySelector('img');
+const txtArea = document.querySelector('textarea');
+
+sel.addEventListener('change', build);
+
+inpAll.forEach(function(item) {
+  item.addEventListener('change', build);
+});
+
+const clean = str => {
+  return str.replace('#', '');
+};
+
+const spacers = str => {
+  return str.split(' ').join('+');
+};
+
+function build(e) {
+  let i = {};
+  i.size = sel.value;
+  i.text = spacers(inpAll[0].value);
+  i.bgClr = clean(inpAll[1].value);
+  i.txtClr = clean(inpAll[2].value);
+  i.path = `http://via.placeholder.com/${i.size}/${i.bgClr}/${i.txtClr}?text=${
+    i.text
+  }`;
+  myImg.src = i.path;
+  txtArea.value = i.path;
+  txtArea.select();
+  txtArea.focus();
+  console.log(i);
+}
